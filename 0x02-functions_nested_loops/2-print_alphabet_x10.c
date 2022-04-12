1dd21ae8de41f9ab101d5f1f7aabaa67052df52e
Betty Girma <bettygcodes@gmail.com>
2022-04-12 11:45:09 -0700

    Task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,14 @@
-
 #include "main.h"
 
 /**
  * print_alphabet_x10 - print alphabet
  *
  * Return: Always 0.
- */
-void print_alphabet_x10(void)
-{
-int alphabet;
-int count;
+ */ void print_alphabet_x10(void)
+ {
+ int alphabet;
+ int count;
+
 
 
 count = 0;
@@ -25,3 +24,4 @@ _putchar('\n');
 }
 
 
+}
