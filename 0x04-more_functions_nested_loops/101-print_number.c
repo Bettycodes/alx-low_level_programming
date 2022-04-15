9ae4cf1da0e222da41d1105457e8791002012811
Betty Girma <bettygcodes@gmail.com>
2022-04-15 05:43:42 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,4 +5,42 @@
 * Return: Always 0.
 */
 void print_number(int n)
+
 {
+
+
+int i;
+int z;
+int d;
+int p;
+
+if (size > 0)
+{
+d = size - l;
+for (i = 0; i < size ; i++)
+{
+for (z= d; z > 0 ; z--)
+{
+_putchar (' ');
+}
+
+for(p=0; p <= i; p++)
+{
+_putchar (35);
+}
+
+d--;
+
+putchar ('\n');
+
+
+
+}
+
+}
+
+else
+{
+_putchar ('\n');
+}
+}
